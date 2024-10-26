00bd0e6e6f76ad4b6b78ee9c3a529ae01180df52
Luis Landkoer <angellandkoer3@gmail.com>
2024-10-26 19:05:23 -0400

    CONFIGURACION DE LOGIN Y RUTAS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 import { CSpinner, useColorModes } from '@coreui/react'
@@ -18,6 +18,7 @@ const App = () => {
   const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
   const storedTheme = useSelector((state) => state.theme)
 
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.href.split('?')[1])
     const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
@@ -33,7 +34,7 @@ const App = () => {
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Suspense
         fallback={
           <div className="pt-3 text-center">
@@ -42,14 +43,15 @@ const App = () => {
         }
       >
         <Routes>
-          <Route exact path="/login" name="Login Page" element={<Login />} />
-          <Route exact path="/register" name="Register Page" element={<Register />} />
-          <Route exact path="/404" name="Page 404" element={<Page404 />} />
-          <Route exact path="/500" name="Page 500" element={<Page500 />} />
-          <Route path="*" name="Home" element={<DefaultLayout />} />
+          <Route path="/home" name='Home' element={<DefaultLayout/>}/>
+          <Route  path="/login" name="Login Page" element={<Login />} />
+          <Route  path="/register" name="Register Page" element={<Register />} />
+          <Route  path="/404" name="Page 404" element={<Page404 />} />
+          <Route  path="/500" name="Page 500" element={<Page500 />} />
+          <Route path="*"  element={<Navigate to="/Login" />} />
         </Routes>
       </Suspense>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
